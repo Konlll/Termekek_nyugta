4b951d7f0abf52f8ff5c32824180f7c5c1a7d243
Géczi Kornél <hungarokornel@gmail.com>
2022-01-15 15:48:40 +0100

    Version 1.0

--- main.py
+++ main.py
@@ -1,64 +1,5 @@
-'''import os
-
-def betolt_szotarak_szotaraba(tarolo):
-
-    fajl = open(file="Terméklista.csv", mode="tr")
-    sorok = fajl.readlines()
-    fajl.close()
-
-    del sorok[0]
-
-    for sor in sorok:
-        mezok = sor.split(";")
-
-        uj_termek = {}
-        uj_termek["kategoria"] = mezok[1]  # kategória
-        uj_termek["gyarto"] = mezok[2]  # gyártó
-        uj_termek["termeknev"] = mezok[3]  # terméknév
-        uj_termek["ar"] = int(mezok[4])  # ár
-        uj_termek["garancia"] = int(mezok[5])  # garancia
-
-        tarolo[mezok[0]] = uj_termek  # cikkszám
-
-def osszegez(tetelek):
-    szum=0
-    # ???
-    return szum
-
-def letezo_termek(tarolo, cikkszam):
-    van_ilyen=False
-    # ???
-    #  Mikor teljesül, az hogy:   van_ilyen=True
-    return van_ilyen
-
-szotarak_szotara = {}
-betolt_szotarak_szotaraba(szotarak_szotara)
-
-nyugta_tetelei = []
-
-os.system('cls')
-keresett_cikkszam = "_"
-while keresett_cikkszam != "":
-
-    print(f"Eddigi tételek száma: {len(nyugta_tetelei)} db, értékük : {osszegez(nyugta_tetelei)} Ft")
-
-    keresett_cikkszam = input("\nKérem a termék cikkszámát! :")
-    if keresett_cikkszam == "":  # befejezés
-        break
-
-    if letezo_termek(szotarak_szotara, keresett_cikkszam):
-#    if letezo_termek(listaban_szotarak, keresett_cikkszam):
-#    if letezo_termek(szotarak_szotara, keresett_cikkszam):
-        mennyiseg = int(input("\nKérem az eladási mennyiséget! :"))
-        eladasi_ar = int(input("Kérem az eladási árat! :"))
-        nyugta_tetelei.append([keresett_cikkszam, mennyiseg, eladasi_ar])
-    else:
-        print("\nHIBA: Nem létező cikkszám!")
-
-print(nyugta_tetelei)'''
-
 import eel
-from random import randint
+from datetime import datetime
 
 eel.init("web")
 
@@ -77,6 +18,7 @@ def read_from_file():
         mezok = sor.split(";")
 
         uj_termek = {}
+        uj_termek["cikkszam"] = mezok[0]
         uj_termek["kategoria"] = mezok[1]  # kategória
         uj_termek["gyarto"] = mezok[2]  # gyártó
         uj_termek["termeknev"] = mezok[3]  # terméknév
@@ -85,5 +27,15 @@ def read_from_file():
 
         tarolo[mezok[0]] = uj_termek  # cikkszám
     return tarolo
+
+@eel.expose
+def create_receipt(products):
+    print(products)
+    now_date = datetime.today().strftime('%Y-%m-%d-%H-%M-%S')
+    with open(f'{now_date}.TEN', 'w', encoding="utf-8") as f:
+        f.write("Gyártó;Darab;Eár\n")
+        for product in products:
+            f.write(f"{product['articleNumber']};{product['piece']};{product['price']}\n")
+
 # Start the index.html file
 eel.start("index.html")
--- web/main.js
+++ web/main.js
@@ -1,5 +1,112 @@
 eel.read_from_file()((items) => {
     for (const item in items) {
-        document.querySelector(".wrapper").innerHTML += items[item]["ar"] + "<br>"
+        option = document.createElement("option")
+        option.value = `${items[item]["cikkszam"]}`
+        option.text = `${items[item]["kategoria"]}      ${items[item]["termeknev"]}`
+        document.querySelector(".select-input").add(option, null)
+    }
+})
+
+const changeSelectValue = (select) => {
+    if (select.value != "start"){
+        document.querySelector(".inputs").style.display = "block"
+    } else{
+        document.querySelector(".inputs").style.display = "none"
+    }
+}
+
+const addProduct = (products, select, piece, price) => {
+    pieceInput = document.querySelector("#db")
+    priceInput = document.querySelector("#price")
+    piece = pieceInput.value != "" ? pieceInput.value : 1
+    price = priceInput.value != "" ? priceInput.value : 5000
+
+    eel.read_from_file()(items => {
+        product = {
+            articleNumber: items[select]["cikkszam"],
+            piece: piece,
+            price: price
+        }
+
+        pieceInput.value = ""
+        priceInput.value = ""
+        console.log(product)
+        products.push(product)
+    })
+}
+
+products = []
+document.querySelector("#save").addEventListener("click", (e) => {
+    e.preventDefault()
+    select = document.querySelector(".select-input").value
+    if(select.value == "start"){
+        alert("Kérem jelöljön ki egy terméket!")
+    } else{
+        addProduct(products, select, piece=1, price=5000)
+        console.log(products)
+        document.querySelector(".select-input").options[0].selected = "selected"
+    }
+})
+
+document.querySelector("#showProducts").addEventListener("click", e => {
+
+    e.preventDefault()
+
+    e.target.style.display = "none"
+
+    document.querySelector(".inputs").style.display = "none"
+    document.querySelector(".product-list").style.display = "none"
+
+    document.querySelector(".added-products").style.display = "block"
+    document.querySelector(".product-summary").style.display = "block"
+    document.querySelector(".receipt").style.display = "block"
+    document.querySelector(".back").style.display = "block"
+
+    document.querySelector(".added-products").innerHTML = ""
+
+    pieceSummary = 0
+    priceSummary = 0
+
+    products.forEach(product => {
+      document.querySelector(".added-products").innerHTML += `
+        <div class="product">
+            <div class="product-title">
+                <h2>Cikszám: ${product.articleNumber} &nbsp; Mennyiség: ${product.piece} db &nbsp; Egységár: ${product.price} Ft</h2>
+            </div>
+        </div>
+    `
+        pieceSummary += parseInt(product.piece)
+        priceSummary += parseInt(product.price) * parseInt(product.piece)
+    })
+
+    document.querySelector(".pieceDb").innerHTML = pieceSummary
+    document.querySelector(".priceDb").innerHTML = priceSummary
+
+})
+
+const back = (e) => {
+    e.preventDefault()
+
+    document.querySelector(".back").style.display = "none"
+    document.querySelector(".inputs").style.display = "none"
+    document.querySelector(".product-list").style.display = "flex"
+
+    document.querySelector(".product-summary").style.display = "none"
+    document.querySelector(".receipt").style.display = "none"
+    document.querySelector("#showProducts").style.display = "block"
+    document.querySelector(".added-products").style.display = "none"
+}
+
+document.querySelector(".back").addEventListener("click", (e) => back(e))
+
+document.querySelector(".receipt").addEventListener("click", e => {
+    e.preventDefault()
+    if(products.length == 0){
+        alert("Üres nyugtát nem hozhat létre!")
+    } else{
+        eel.create_receipt(products)
+        products = []
+        back(e)
+        alert("Nyugta létrehozása sikeres!")
     }
 })
